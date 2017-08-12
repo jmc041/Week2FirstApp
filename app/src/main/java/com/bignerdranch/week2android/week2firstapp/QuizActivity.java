9cc832ee446e140e3f4095e70235d4a01de26514
jacksonmalcolmchaplin <jmc041@student.usc.edu.au>
2017-08-12 14:54:10 +1000

    Layout orientation change complete (working) sav instance state

--- app/src/main/java/com/bignerdranch/week2android/week2firstapp/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/week2android/week2firstapp/QuizActivity.java
@@ -3,6 +3,7 @@ package com.bignerdranch.week2android.week2firstapp;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,17 +14,22 @@ public class QuizActivity extends AppCompatActivity {
     private Button mFalseButton;
     private Button mNextButton;
     private TextView mQuestionTextView;
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
+        if (savedInstanceState != null) {
+            mcurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-
-
-
+        //TRUE BUTTON
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
 
@@ -55,6 +61,46 @@ public class QuizActivity extends AppCompatActivity {
         });
         updateQuestion();
         }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedinstanceState) {
+        super.onSaveInstanceState(savedinstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedinstanceState.putInt(KEY_INDEX, mcurrentIndex);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+
+
+
 
     private void updateQuestion() {
         int question = mQuestionBank[mcurrentIndex].getTextResId();
